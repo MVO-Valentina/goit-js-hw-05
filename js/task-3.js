5d261ee6b5da5267d069c827536f5ddb3d8650c8
valyafrontend1989 <144459911+valyafrontend1989@users.noreply.github.com>
2025-10-20 16:30:08 +0300

    fix: task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,5 @@
-// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
-
-// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
 const sortByDescendingFriendCount = users => {
-  return users.sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
 
 console.log(
@@ -21,7 +15,7 @@ console.log(
       gender: 'female',
     },
     {
-      name: 'Ross Vazquez',
+      name: 'Ross Vazquez', a
       friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
       gender: 'male',
     },
@@ -47,43 +41,6 @@ console.log(
     },
   ])
 );
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
 
 // Залиш цей код для перевірки ментором.
 
